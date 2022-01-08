6d37c8ca303cf67eca8c9dd7a6cae38db4657986
sankethkini <kinisanketh@gmail.com>
2022-01-08 22:58:27 +0530

    added constants file
    constant file contains all the constants which will help during final price calculation

---
+++ inventory/constants/constants.go
@@ -0,0 +1,14 @@
+package constants
+
+//raw
+const RawTax float64 = 12.5
+
+//imported
+const ImportTax float64 = 10
+const Surcharge100 float64 = 5
+const Surcharge200 float64 = 10
+const SurchargeMore float64 = 5
+
+//manufactured
+const ManufacturedTax float64 = 12.5
+const ManufacturedExtra float64 = 2
