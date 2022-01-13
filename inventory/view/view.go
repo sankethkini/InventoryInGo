22fed4d5885a3120517bbbf328df1a632eb1c090
sankethkini <kinisanketh@gmail.com>
2022-01-13 11:49:47 +0530

    modified view package, adapted more modularised coding, exported only required functions

--- inventory/view/view.go
+++ inventory/view/view.go
@@ -5,44 +5,76 @@ import (
 	"inventory/inventory/commands"
 )
 
-var curCommand commands.Command
-
-func GetDetails() {
+//getDeatils function helps to get details from user
+func getDetails() {
 	var name, typ string
 	var quantity int
 	var price float64
+
 	fmt.Println("Enter name of the product")
 	fmt.Scanf("%s", &name)
+
 	fmt.Println("Enter type of product raw,imported,manufactured")
 	fmt.Scanf("%s", &typ)
+
 	fmt.Println("Enter the quantity")
 	fmt.Scanf("%d", &quantity)
+
 	fmt.Println("Enter the Price")
 	fmt.Scanf("%f", &price)
-	UserInput(1, name, quantity, price, typ)
+
+	RunCommand(1, name, quantity, price, typ)
 }
 
-func Display(cur []map[string]string) {
+//display function displays the result of operation to user
+func display(cur []map[string]interface{}) {
 
+	fmt.Printf("------------------------\n")
 	for _, val := range cur {
-		fmt.Println("Name :", val["name"], "Price :", val["price"], "Tax :", val["tax"], "Total :", val["total"])
+		for key, val := range val {
+			fmt.Printf("%v ----> %v\n", key, val)
+		}
+		fmt.Printf("------------------------\n")
 	}
 }
-func UserInput(curInput int, name string, quantity int, price float64, typ string) {
+
+//executeCommand function executes command selected by user
+func executeCommand(cmd commands.Command) {
+	res, err := cmd.Execute()
+	if err != nil {
+		fmt.Println(err)
+	}
+	display(res)
+}
+
+//RunCommand function runs the right command for perticular input
+func RunCommand(curInput int, name string, quantity int, price float64, typ string) {
+
+	var curCommand commands.Command
+	var err error
 	if curInput == 1 {
-		add := commands.Add{}
-		add.Init(name, quantity, price, typ)
-		curCommand = &add
-		cur := curCommand.Execute()
-		Display(cur)
+		curCommand, err = commands.NewAddCommand(name, quantity, price, typ)
 	} else if curInput == 2 {
-		curCommand = &commands.Display{}
-		curCommand.Execute()
-		cur := curCommand.Execute()
-		Display(cur)
+		curCommand, err = commands.NewDisplayCommand()
 	} else {
-		curCommand = &commands.Exit{}
-		cur := curCommand.Execute()
-		Display(cur)
+		curCommand, err = commands.NewExitCommand()
+	}
+	if err != nil {
+		fmt.Println(err)
+
+	} else {
+		executeCommand(curCommand)
+	}
+
+}
+
+func MenuForUser() {
+	fmt.Println("1. add more items 2. display all 3. exit")
+	var cur int
+	fmt.Scanf("%d", &cur)
+	if cur == 1 {
+		getDetails()
+	} else {
+		RunCommand(cur, "", 0, 0, "")
 	}
 }
